99ae1e1c1b8a910e5a9a62d61749bcd3c03cdca0
Anthony Mobile <arjstreett@fmail.com>
2020-09-11 01:20:12 -0400

    new button resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,15 +2,18 @@ import React from "react";
 import Resume1 from "../images/resume1.jpg"
 import Resume2 from "../images/resume2.jpg"
 import "../styles/Resume.css"
+import ControlPointIcon from '@material-ui/icons/ControlPoint';
+
 
 
 
 function Resume() {
   return (
-    <div className="resume__container">
+    <div className="resume">
+        <div className="resume__container"><img src={Resume1}/>
+        <img src={Resume2}/></div>
         
-        <img src={Resume1}/>
-        <img src={Resume2}/>
+        <a href="https://drive.google.com/file/d/1cc7_gMTGCky6S5hqEuKZQypN00FV_Q5b/view?usp=sharing" rel="noopener noreferrer" target="_blank" download><button>Download <ControlPointIcon/></button></a>
     </div>
   );
 }
